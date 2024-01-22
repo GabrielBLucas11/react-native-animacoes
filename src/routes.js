a006ced7400e3a96cdd72c6f7d496c7436edeb07
GabrielBLucas11 <gabrielbl79@gmail.com>
2024-01-22 15:59:45 -0400

    Projeto Base

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Tab = createNativeStackNavigator();
+
+import Onboarding from './views/Onboarding';
+import Principal from './views/Principal';
+import Detalhes from './views/Detalhes';
+
+export default function Rotas() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Onboarding" component={Onboarding} options={{ headerShown: false }}/>
+        <Tab.Screen name="Principal" component={Principal} options={{ headerShown: false }}/>
+        <Tab.Screen name="Detalhes" component={Detalhes}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
