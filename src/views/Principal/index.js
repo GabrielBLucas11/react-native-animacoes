15c04987f7595cf3b9ea7483dd2b4275b13a563d
GabrielBLucas11 <gabrielbl79@gmail.com>
2024-01-23 10:32:09 -0400

    Add shimmer effect and timer to Principal view

--- src/views/Principal/index.js
+++ src/views/Principal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { InformacoesUsuario } from "../../components/InformacoesUsuario";
 import { CardConsulta } from "../../components/CardConsulta";
@@ -6,30 +6,53 @@ import { TelaDeFundo } from "../../components/TelaDeFundo";
 import soniaFoto from "../../assets/sonia.png";
 import pacientes from "./pacientes";
 import styles from "./styles";
+import { CardConsultaShimmerEffect } from "../../components/CardConsultaShimmerEffect";
+import { InformacoesUsuarioShimmerEffect } from "../../components/InformacoesUsuarioShimmerEffect";
 
 export default function Principal({ navigation }) {
+  const [tempo, setTempo] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTempo(true);
+    }, 3000);
+  }, [])
 
   return (
     <TelaDeFundo>
     <View style={styles.container}>
-      <InformacoesUsuario 
-        nome="Olá Sônia"
-        detalhes="Mais 4 consultas hoje"
-        foto={soniaFoto}
-      />
 
-      <Text style={styles.texto}>Hoje</Text>
+      { tempo ? 
+      <>
+        <InformacoesUsuario 
+          nome="Olá Sônia"
+          detalhes="Mais 4 consultas hoje"
+          foto={soniaFoto}
+          />
 
-      <FlatList
-        data={pacientes}
-        keyExtractor={item => String(item.id)}
-        renderItem={({ item }) => 
-        <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
-          <CardConsulta {...item} />
-        </TouchableOpacity>
-        }
-        showsVerticalScrollIndicator={false}
-      />
+        <Text style={styles.texto}>Hoje</Text>
+
+        <FlatList
+          data={pacientes}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => 
+          <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
+            <CardConsulta {...item} />
+          </TouchableOpacity>
+          }
+          showsVerticalScrollIndicator={false}
+          />
+      </>
+      : 
+        <>
+          <InformacoesUsuarioShimmerEffect />
+          <CardConsultaShimmerEffect />
+          <CardConsultaShimmerEffect />
+          <CardConsultaShimmerEffect />
+          <CardConsultaShimmerEffect />
+        </>
+      }
+
     </View> 
     </TelaDeFundo>
   );
